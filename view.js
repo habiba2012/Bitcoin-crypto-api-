61280ebd9c85c961cffceb85ac3d2ff05ec4434e
Habiba Akhter <habibaakhter2012@gmail.com>
2019-10-19 23:12:23 +0200

    Add files via upload

---
+++ view.js
@@ -0,0 +1,36 @@
+class View {
+    constructor() {
+        this.btnGetApi = document.querySelector("#btnGetApi");
+
+        this.getApi();
+    }
+
+
+    getApi() {
+        this.btnGetApi.addEventListener("click", () => {
+            fetch('https://apiv2.bitcoinaverage.com/constants/exchangerates/global')
+                .then(res => { return res.json() })
+                .then((data) => {
+                    console.log(Object.keys(data.rates));
+
+                    let output = "<h2> Posts </h2>";
+
+                    let rates = Object.keys(data.rates);
+
+                    rates.forEach(rate => {
+                        output += `
+                            <h3> rate: ${data.rates[rate].name}</h3>
+                            <h3> rate: ${data.rates[rate].rate}</h3>
+                            `
+                    });
+                    this.outputPosts.innerHTML = output;
+
+
+                })
+        })
+
+    }
+
+}
+
+export default View
